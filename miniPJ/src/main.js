ead83bfa671f97f785a81fe53f0429cfc3c87f75
simwh123 <tlatlek@naver.com>
2024-05-20 12:01:12 +0900

    2024년5월20일 중간저장

--- miniPJ/src/main.js
+++ miniPJ/src/main.js
@@ -1,85 +1,24 @@
-btn4.onclick = function(event) {
-    event.preventDefault();  
-    btn1.src = "img/blue_p.png";
-    btn2.src = "img/blue_s.png";
-    btn3.src = "img/blue_t.png";
-}
-btn5.onclick = function(event) {
-    event.preventDefault();  
-    btn1.src = "img/pink_p.png";
-    btn2.src = "img/pink_s.png";
-    btn3.src = "img/pink_t.png";
-}
-btn6.onclick = function(event) {
-    event.preventDefault();  
-    btn1.src = "img/yellow_p.png";
-    btn2.src = "img/yellow_s.png";
-    btn3.src = "img/yellow_t.png";
+function jsonLoad(){
+    return fetch('data/data.json').then(res => res.json()).then(data => data.items);
 }
 
-// 각 이미지와 버튼 요소를 가져옵니다.
-const bluePImage = document.getElementById('btn1');
-const pinkPImage = document.getElementById('btn2');
-const yellowPImage = document.getElementById('btn3');
-const mainOption = document.getElementById('mainoption');
-
-// 클릭된 이미지에 해당하는 요소만을 표시하는 함수입니다.
-function showSelectedProduct(imageSrc) {
-    // main 영역의 모든 product 요소를 확인합니다.
-    mainOption.querySelectorAll('.product').forEach(product => {
-        // 각 product 요소에서 img 요소를 가져옵니다.
-        const img = product.querySelector('img');
-        // 현재 이미지의 src와 클릭된 이미지의 src를 비교하여 일치하면 해당 요소를 표시합니다.
-        if (img.src === imageSrc) {
-            product.style.display = 'block';
-        } else {
-            // 일치하지 않으면 해당 요소를 숨깁니다.
-            product.style.display = 'none';
-        }
-    });
+function putItems(items) {
+    const ul = document.querySelector('.items');
+    ul.innerHTML = items.map(item => {
+        return `<li class="item">
+            <img src="${item.image}" alt="${item.type}" class="item-thumbnail">
+            <span class="item-description">${item.gender}, ${item.size}</span>
+          </li>`;
+    }).join('\n');
 }
 
-// 각 이미지를 클릭할 때마다 해당 이미지와 동일한 이미지를 가진 요소만을 표시하도록 설정합니다.
-bluePImage.addEventListener('click', function() {
-    showSelectedProduct(bluePImage.src);
-});
 
-pinkPImage.addEventListener('click', function() {
-    showSelectedProduct(pinkPImage.src);
-});
+function onLoad(){
+    jsonLoad().then(items => putItems(items));
+}
+onLoad();
 
-yellowPImage.addEventListener('click', function() {
-    showSelectedProduct(yellowPImage.src);
-});
-
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    // JSON 파일을 가져옵니다.
-    fetch("data/data.json")
-        .then(response => response.json())
-        .then(data => {
-            // 제품 목록을 가져옵니다.
-            const items = data.items;
-
-            // 제품 목록을 순회하며 HTML에 추가합니다.
-            const productList = document.querySelector(".product-list");
-
-            items.forEach(item => {
-                const li = document.createElement("li");
-                li.classList.add("product");
-
-                const img = document.createElement("img");
-                img.src = item.image;
-                img.width = "50px";
-
-                const span = document.createElement("span");
-                span.textContent = `${item.gender}, ${item.size}`;
-
-                li.appendChild(img);
-                li.appendChild(span);
-                productList.appendChild(li);
-            });
-        })
-        .catch(error => console.error("Error fetching data:", error));
-});
+function filter(method,filterText){
+    jsonLoad().then(items => items.filter(item => item[method] === filterText))
+        .then(filterItem => putItems(filterItem));
+}
